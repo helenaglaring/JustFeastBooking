c17bce828b0bb2560ee6b38c08e8b8568f59791f
helenaglaring <55111550+helenaglaring@users.noreply.github.com>
2020-05-08 08:12:12 +0200

    app.js med opdateringer

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-/* Module dependencies */
+// Kilde: https://medium.com/@selvaganesh93/how-node-js-middleware-works-d8e02a936113
+/*--app.js--------------------------APP configuration -----------------------------------------------------*/
+
+
+// Module dependencies
 const express = require('express');
 const path = require('path');
 
-/* Import middleware */
-const bodyParser = require('body-parser');
+// Importerer third-party middleware
 const cookieParser = require('cookie-parser');
-//const session = require('express-session');
+const bodyParser = require('body-parser');
 
-/* Import routes  */
-const routes = require('./server/routes/index');
-const userRoutes = require('./server/routes/user');
-const cartRoutes = require('./server/routes/cart');
-const orderRoutes = require('./server/routes/order');
 
-// Express-session and connect-pg-simple to store data in session
-// https://openbase.io/js/connect-pg-simple
+// Express-session and connect-pg-simple to store data in session. Kilde: https://openbase.io/js/connect-pg-simple
 const pg = require('pg')
     , session = require('express-session')
     , pgSession = require('connect-pg-simple')(session);
+
+const flash = require('connect-flash');
+
+// Import routehandlers
+const routes = require('./server/routes/index');
+/*
+const userRoutes = require('./server/routes/user');
+const cartRoutes = require('./server/routes/cart');
+const orderRoutes = require('./server/routes/order');*/
+
 
 const pgPool = new pg.Pool({
     // Insert pool options here
@@ -28,30 +35,30 @@ const pgPool = new pg.Pool({
     password: "password",
 });
 
-//const pgSession = require('connect-pg-simple')(session);
-//const pool = require('./server/db/db');
 
-const flash = require('connect-flash');
-const passport = require('passport');
-
-/* Server configuration */
+// Server configuration
 const app = express ();
-const port = 3000;
+const port = process.env.PORT || 3000; //kilde: https://www.youtube.com/watch?v=pKd0Rpw7O48
 
 
-
-
-/* All environments */
 
 // Set views and view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Set up public files
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+//Cookie and body-parser middleware
+//https://medium.com/@ratan_kumar/a-sweet-tale-of-cookies-and-sessions-with-node-js-5fe255b702ac
+
+app.use(cookieParser());
+
 // Apply cookie session middleware
-app.use(session({ // Using session to flash messages
+
+app.use(session({
    store: new pgSession({
         pool: pgPool,
         tableName: 'session'
@@ -62,30 +69,47 @@ app.use(session({ // Using session to flash messages
     cookie: { maxAge: 180 * 60 * 1000 } // How long a session should live in ms, 1 hour.
 }));
 
-//Cookie and body-parser middleware
-app.use(cookieParser());
+
 app.use(bodyParser.json());
+
 app.use(
     bodyParser.urlencoded ({
         extended: true,
     })
 );
-
+// Using connect-flash with session to flash messages
 app.use(flash());
-//app.use(validator());
-app.use(passport.initialize());
-app.use(passport.session());
 
 
-/* Endpoints  */
-app.use('/', routes);
+// Endpoints
+app.use('/', routes)
+
 /*
 app.use('/user', userRoutes);
 app.use('/cart', cartRoutes);
 app.use('/order', orderRoutes);
 */
 
-/* Connect to server */
+
+// Error-handling middleware. Kilde: https://www.hacksparrow.com/webdev/express/custom-error-pages-404-and-500.html
+// Handle 404
+app.use(function(req, res) {
+    res.status(404);
+    res.render('error', {title: '404: File Not Found'});
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+    console.log(error);
+    res.status(500);
+    res.render('error', {
+        title:'500: Internal Server Error'});
+});
+
+
+// Connect to server
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 });
+
+
