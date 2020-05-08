076065ab3492922967ed7c03dd4fa515759ac74b
helenaglaring <55111550+helenaglaring@users.noreply.github.com>
2020-05-08 08:23:34 +0200

    Delete homeController.js

--- server/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req , res) => {
-    res.render('home', {
-        title: 'Home',
-        messages: {
-            success: req.flash('success'),
-            error: req.flash('error')
-        }
-    });
-};
