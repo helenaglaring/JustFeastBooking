90e9b75f7152617529afd70ee81ef54139d5a025
helenaglaring <55111550+helenaglaring@users.noreply.github.com>
2020-05-08 12:06:10 +0200

    Delete errorPage.js

--- controllers/errorPage.js
+++
@@ -1,6 +0,0 @@
-
-/*----------------------------------- ERROR PAGE------------------------------------------------------*/
-// Rendering error page for http-status 404 og 500 errors
-module.exports = (req,res)=>{
-    res.render('error');
-};
